39cd4a2b28c2ce767f88ea2c5ed5f74ce86ef465
POWER <clay.degruchy@gmail.com>
2021-05-25 19:01:52 +0200

    added basic movement and telemetry

--- scripts/module.js
+++ scripts/module.js
@@ -1,37 +1,92 @@
-let horVisible
-let vertVisible
-var myWorker
+var defaults = {
+  frequency: 2000,
+}
+
+
+var points = [{
+    x: 384,
+    y: 216
+  }, {
+    x: 484,
+    y: 116
+  }, {
+    x: 284,
+    y: 216
+  },
+
+
+]
+
+
+var last = 0
+
 
 Hooks.once('init', async function() {
 
+  window.modelMove = window.modelMove || {
+    ...defaults
+  }
+
+  window.modelMove.update = async function() {
+    var newCoord = points[last]
+    last += 1
+    if (last > 2) last = 0
+
+    // console.log(this.locations);
+    // console.log(this.horVisible);
+    // console.log(this.vertVisible);
+
+    for (let [name, location] of Object.entries(this.locations)) {
+      console.log(name, location);
+    }
+
+
+    var token = this.findToken("test")
+    this.moveToken(token, {
+      x: 0,
+      y: 0
+    })
+  }
+
+  window.modelMove.findToken = function(name) {
+    return canvas.tokens.children[0].children.find(t => t.name == name)
+  }
+
+  window.modelMove.moveToken = function(token, coords) {
+    //Compensate for the difference between the center of the token and the top-left of the token, and compensate for token size
+    // coords.x -= token.hitArea.width / 2 + (token.data.width - 1) * canvas.scene.data.grid / 2;
+    // coords.y -= token.hitArea.height / 2 - (token.data.height - 1) * canvas.scene.data.grid / 2;
+
+    let gridCoords = canvas.grid.getCenter(coords.x - canvas.scene.data.grid / 2, coords.y - canvas.scene.data.grid / 2)
+
+    let newCoords = {
+      x: (gridCoords[0] + canvas.scene.data.grid / 2),
+      y: (gridCoords[1] + canvas.scene.data.grid / 2)
+    }
+    let newCoords2 = {
+      x: (newCoords.x + canvas.scene.data.grid / 2),
+      y: (newCoords.y + canvas.scene.data.grid / 2)
+    }
+
+    if (this.storedPosition != newCoords && (token.checkCollision(newCoords2, true) == false || game.user.isGM)) {
+      token.update(newCoords);
+      this.storedPosition = newCoords;
+      this.currentPosition = newCoords;
+    }
+
+  }
 
 
 
 });
 
 Hooks.once('ready', async function() {
-    let horVisible = screen.width / canvas.scene._viewPosition.scale;
-    let vertVisible = screen.height / canvas.scene._viewPosition.scale;
-
-    myWorker = new Worker("./scripts/worker.js", {
-        type: "module"
-    });
-
-    myWorker.onmessage = function(e) {
-        result.textContent = e.data;
-        console.log('Message received from worker');
-    }
-
-
+  window.modelMove.horVisible = screen.width / canvas.scene._viewPosition.scale;
+  window.modelMove.vertVisible = screen.height / canvas.scene._viewPosition.scale;
 });
-
 
 
 Hooks.on('canvasPan', async function(canvas, canvasPan) {
-	fetch('./scripts/worker.js')
-	.then(console.log)
-    console.log(myWorker)
-    horVisible = screen.width / canvas.scene._viewPosition.scale;
-    vertVisible = screen.height / canvas.scene._viewPosition.scale;
+  window.modelMove.horVisible = screen.width / canvas.scene._viewPosition.scale;
+  window.modelMove.vertVisible = screen.height / canvas.scene._viewPosition.scale;
 });
-
--- scripts/worker.js
+++ scripts/worker.js
@@ -1,4 +1,19 @@
-console.log('worker started')
-setInterval(function() {
-    console.log('test')
-}, 1000);
+Hooks.once('ready', async function() {
+  console.log('worker started')
+
+
+  var last = 0
+  setInterval(async function() {
+
+    if (!window.modelMove.tick) window.modelMove.tick = 0
+    window.modelMove.tick = window.modelMove.tick += 1
+
+
+    window.modelMove.locations = await fetch("http://localhost:5000/api/update.json")
+      .then(r => r.json())
+
+    await window.modelMove.update()
+
+  }, window.modelMove.frequency || 1000);
+
+});
