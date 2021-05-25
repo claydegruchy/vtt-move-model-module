2bce5c386208e5ed60b4c73cea50bf7203d85170
Clay <clay.degruchy+github@gmail.com>
2021-05-25 17:13:14 +0200

    mes

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,37 @@
+let horVisible
+let vertVisible
+var myWorker
+
 Hooks.once('init', async function() {
+
+
+
 
 });
 
 Hooks.once('ready', async function() {
+    let horVisible = screen.width / canvas.scene._viewPosition.scale;
+    let vertVisible = screen.height / canvas.scene._viewPosition.scale;
+
+    myWorker = new Worker("./scripts/worker.js", {
+        type: "module"
+    });
+
+    myWorker.onmessage = function(e) {
+        result.textContent = e.data;
+        console.log('Message received from worker');
+    }
+
 
 });
+
+
+
+Hooks.on('canvasPan', async function(canvas, canvasPan) {
+	fetch('./scripts/worker.js')
+	.then(console.log)
+    console.log(myWorker)
+    horVisible = screen.width / canvas.scene._viewPosition.scale;
+    vertVisible = screen.height / canvas.scene._viewPosition.scale;
+});
+
---
+++ scripts/worker.js
@@ -0,0 +1,4 @@
+console.log('worker started')
+setInterval(function() {
+    console.log('test')
+}, 1000);
